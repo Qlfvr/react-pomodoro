c78a330ac51a848826acfc77ea475c4d8c71c7a5
Quentin <lefevre.quentin@gmail.com>
2020-05-13 17:21:49 +0200

    add auto-reset when timer === 0

--- src/app.js
+++ src/app.js
@@ -8,22 +8,27 @@ class App extends React.Component {
 
         this.state = {
             seconds: 1500,
+            play: false,
         };
 
         this.timerIncrement = this.timerIncrement.bind(this);
         this.timerDecrement = this.timerDecrement.bind(this);
         this.timerReset = this.timerReset.bind(this);
+        this.timerSwitch = this.timerSwitch.bind(this);
     }
 
     tick() {
         this.setState(state => {
-            if (state.seconds > 0) {
+            if (state.seconds > 0 && state.play === true) {
                 return {
                     seconds: state.seconds - 1,
                 };
             }
             return state;
         });
+        if (this.state.seconds === 0) {
+            this.timerReset();
+        }
     }
     componentDidMount() {
         this.interval = setInterval(() => this.tick(), 1000);
@@ -35,23 +40,40 @@ class App extends React.Component {
 
     // button on clicks functions
 
+    timerSwitch() {
+        this.setState(prevState => ({
+            play: !prevState.play,
+        }));
+    }
+
     timerIncrement() {
         this.setState(prevState => ({seconds: prevState.seconds + 300}));
     }
     timerDecrement() {
-        this.setState(prevState => ({seconds: prevState.seconds - 300}));
+        if (this.state.seconds > 300) {
+            this.setState(prevState => ({seconds: prevState.seconds - 300}));
+        } else {
+            this.setState(() => ({seconds: 0}));
+        }
     }
     timerReset() {
-        this.setState(() => ({seconds: 1500}));
+        this.setState(() => ({
+            seconds: 1500,
+            play: false,
+        }));
     }
 
     render() {
         return (
-            <div>
+            <div className={"main"}>
                 <Timer seconds={this.state.seconds} />
-                <Button handleClick={this.timerIncrement} label={"+"} />
+                <Button handleClick={this.timerIncrement} label={"+5"} />
+                <Button
+                    handleClick={this.timerSwitch}
+                    label={this.state.play === true ? " Pause" : "Play"}
+                />
                 <Button handleClick={this.timerReset} label={" Reset "} />
-                <Button handleClick={this.timerDecrement} label={" - "} />
+                <Button handleClick={this.timerDecrement} label={" -5 "} />
             </div>
         );
     }
