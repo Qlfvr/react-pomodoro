c70b6444a9bdc3db5792ea13314de58a766e97bb
Quentin <lefevre.quentin@gmail.com>
2020-05-13 12:12:53 +0200

    Create the functions to add time, substract time, reset timer

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
 import React from "react";
 import Timer from "./components/timer";
+import Button from "./components/button";
 
 class App extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            seconds: 5,
+            seconds: 1500,
         };
+
+        this.timerIncrement = this.timerIncrement.bind(this);
+        this.timerDecrement = this.timerDecrement.bind(this);
+        this.timerReset = this.timerReset.bind(this);
     }
 
     tick() {
-        this.setState((state) => {
+        this.setState(state => {
             if (state.seconds > 0) {
                 return {
                     seconds: state.seconds - 1,
                 };
-            } return state
+            }
+            return state;
         });
     }
-
     componentDidMount() {
         this.interval = setInterval(() => this.tick(), 1000);
     }
@@ -28,11 +33,25 @@ class App extends React.Component {
         clearInterval(this.interval);
     }
 
+    // button on clicks functions
+
+    timerIncrement() {
+        this.setState(prevState => ({seconds: prevState.seconds + 300}));
+    }
+    timerDecrement() {
+        this.setState(prevState => ({seconds: prevState.seconds - 300}));
+    }
+    timerReset() {
+        this.setState(() => ({seconds: 1500}));
+    }
+
     render() {
         return (
             <div>
-                <Timer seconds={this.state.seconds}  />
-            <Timer />
+                <Timer seconds={this.state.seconds} />
+                <Button handleClick={this.timerIncrement} label={"+"} />
+                <Button handleClick={this.timerReset} label={" Reset "} />
+                <Button handleClick={this.timerDecrement} label={" - "} />
             </div>
         );
     }
---
+++ src/components/button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button(props) {
+    return (
+        <button type={"button"} onClick={props.handleClick}>
+            {props.label}
+        </button>
+    );
+}
+
+export default Button;
