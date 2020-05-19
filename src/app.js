180842b4c971f810659f98a9ed29c2dfbae0c9af
Quentin <lefevre.quentin@gmail.com>
2020-05-19 13:50:21 +0200

    add break feature and some styling to it

--- src/app.js
+++ src/app.js
@@ -3,37 +3,47 @@ import Timer from "./components/timer";
 import Button from "./components/button";
 import TaskInput from "./components/task-input";
 
+const defaultTimer = 5; //default timer time in seconds
+const defaultBreak = 2; //default break time in seconds
+
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
             settingUp: true,
-            seconds: 1500,
-            maxValue: 1500, // temps maximum du compteur
+            seconds: 5,
+            maxValue: defaultTimer, // temps maximum du compteur
             play: false,
+            wasBreak: false,
         };
 
         this.timerIncrement = this.timerIncrement.bind(this);
         this.timerDecrement = this.timerDecrement.bind(this);
         this.timerReset = this.timerReset.bind(this);
         this.timerSwitch = this.timerSwitch.bind(this);
+        this.timerBreak = this.timerBreak.bind(this);
     }
 
     tick() {
-        this.setState((state) => {
+        this.setState(state => {
             if (state.seconds > 0 && state.play === true) {
                 return {
                     seconds: state.seconds - 1,
                 };
-            } else if (this.state.seconds === 0) {
-                alert("test");
-                clearInterval(this.interval);
-
-                return state;
             }
 
             return state;
         });
+
+        if (this.state.seconds === 0) {
+            clearInterval(this.interval);
+
+            if (this.state.wasBreak === false) {
+                this.timerBreak();
+            } else {
+                this.timerReset();
+            }
+        }
     }
 
     timerSwitch() {
@@ -56,14 +66,14 @@ class App extends React.Component {
     }
 
     timerIncrement() {
-        this.setState((prevState) => ({
+        this.setState(prevState => ({
             seconds: prevState.seconds + 300,
             maxValue: prevState.seconds + 300,
         }));
     }
     timerDecrement() {
         if (this.state.seconds >= 300) {
-            this.setState((prevState) => ({
+            this.setState(prevState => ({
                 seconds: prevState.seconds - 300,
                 maxValue: prevState.seconds - 300,
             }));
@@ -72,20 +82,45 @@ class App extends React.Component {
     timerReset() {
         clearInterval(this.interval);
         this.setState(() => ({
-        
-            seconds: 1500,
-            maxValue: 1500,
+            seconds: defaultTimer,
+            maxValue: defaultTimer,
             play: false,
             settingUp: true,
+            wasBreak: false,
+        }));
+    }
+
+    timerBreak() {
+        this.setState(() => ({
+            seconds: defaultBreak,
+            maxValue: defaultBreak,
+            play: false,
+            wasBreak: true,
         }));
     }
 
     render() {
-        console.log("un rendu a été fait");
+        if (this.state.wasBreak === true) {
+            document.body.classList.add("bg-break");
+        } else {
+            document.body.classList.remove("bg-break");
+        }
+
+        let reset = "";
+
+        if (this.state.wasBreak === false) {
+            reset = "Reset";
+        } else {
+            reset = "Skip";
+        }
 
         return (
             <div className={"main"}>
-                <TaskInput />
+                {this.state.wasBreak === false ? (
+                    <TaskInput />
+                ) : (
+                    <h1>{"Take five Dave !"}</h1>
+                )}
 
                 <Timer
                     seconds={this.state.seconds}
@@ -103,9 +138,9 @@ class App extends React.Component {
 
                 <Button
                     handleClick={this.timerSwitch}
-                    label={this.state.play === true ? " Pause" : "Play"}
+                    label={this.state.play === true ? " Pause" : "Go"}
                 />
-                <Button handleClick={this.timerReset} label={" Reset "} />
+                <Button handleClick={this.timerReset} label={reset} />
 
                 {this.state.settingUp ? (
                     <Button handleClick={this.timerDecrement} label={" -5 "} />
